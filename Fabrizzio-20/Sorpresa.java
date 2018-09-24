e48d97ac32052073c112d3a587d82be002776652
Fabrizzio RIvera <frivera@nevicad.com>
2018-09-24 09:29:03 -0600

    Sorpresa terminado!!

---
+++ Fabrizzio-20/Sorpresa.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javafx.scene.shape.Line;
+
+public class Sorpresa {
+
+	private static final String FILENAME = "file.txt";
+	public static void main(String[] args) {
+		BufferedReader br = null;
+        FileReader fr = null;
+        ArrayList<Integer> lines=new ArrayList<Integer>();
+		try {			
+			fr = new FileReader(FILENAME);
+			br = new BufferedReader(fr);
+
+			String sCurrentLine;
+
+			while ((sCurrentLine = br.readLine()) != null) {
+                System.out.println(sCurrentLine);
+                
+                lines.add(Integer.parseInt(sCurrentLine));
+            }
+            
+            for (int i = lines.get(0); i < lines.get(1); i++){
+                if (i%lines.get(2) == 0){
+                    System.out.println(i);
+                }
+            }
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+				if (fr != null)
+					fr.close();
+
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
